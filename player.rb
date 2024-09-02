4db7d52653d55a37337eca858d6303d9e3a29490
Somto Ufondu <somtoufondu@gmail.com>
2024-09-02 12:22:50 -0600

    Change the self. to @

--- player.rb
+++ player.rb
@@ -2,31 +2,19 @@ class Player
   attr_reader :name, :lives
 
   def initialize(name)
-    self.name = name
-    self.lives = 3
+    @name = name
+    @lives = 3
   end
 
   def lose_life
-    self.lives -= 1
+    @lives -= 1
   end
 
   def alive?
-    self.lives > 0
+    @lives > 0
   end
 
   def display_status
     puts "#{name}: #{lives}/3"
   end
 end
-
-player1 = Player.new("Player 1")
-
-puts player1.name
-puts player1.lives
-player1.lose_life
-puts player1.lives
-puts player1.alive?
-player1.lose_life
-player1.lose_life
-player1.display_status
-puts player1.alive?
