dbb8b2b5baa53e9e910d087f9626041cd195341b
Somto Ufondu <somtoufondu@gmail.com>
2024-09-02 10:03:13 -0600

    Build Player class with all the requirements

---
+++ player.rb
@@ -0,0 +1,32 @@
+class Player
+  attr_accessor :name, :lives
+
+  def initialize(name)
+    self.name = name
+    self.lives = 3
+  end
+
+  def lose_life
+    self.lives -= 1
+  end
+
+  def alive?
+    self.lives > 0
+  end
+
+  def display_status
+    puts "#{name} has #{lives} lives left"
+  end
+end
+
+player1 = Player.new("Player 1")
+
+puts player1.name
+puts player1.lives
+player1.lose_life
+puts player1.lives
+puts player1.alive?
+player1.lose_life
+player1.lose_life
+player1.display_status
+puts player1.alive?
