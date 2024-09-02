1219300fa61c316a78c9e00a2eeb3cd75a47c37b
Somto Ufondu <somtoufondu@gmail.com>
2024-09-02 11:17:18 -0600

    Build Question class with all the requirements

---
+++ question.rb
@@ -0,0 +1,15 @@
+class Question
+  attr_reader :num1, :num2, answer
+
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @answer = num1 + num2
+  end
+
+  def ask_question(player)
+    puts "#{player.name}, what is #{@num1} + #{@num2}?"
+    player_answer = gets.chomp.to_i
+    player_answer == @answer
+  end    
+end
